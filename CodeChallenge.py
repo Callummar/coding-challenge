32706052adc09ddd5b10077e9b18983cf98cf49f
Callum Mar <30305310+Callummar@users.noreply.github.com>
2021-03-06 14:30:30 +1300

    Update CodeChallenge.py
    Update FilterData input parameter to list. Create filter parameters that are a list of dictionaries.
    Perform Calcuations

--- CodeChallenge.py
+++ CodeChallenge.py
@@ -5,22 +5,65 @@ def main():
     fileContents = ReadJsonFile(jsonFileName)
     fileData = fileContents["data"]
     
-    
-    revenueFilteredData =  FilterData("account_category","revenue",fileData)
-    expensesFilteredData = FilterData("account_category","expense",fileData)
+    #Calcualte Revenue
+    revenueFilter = [{"account_category":["revenue"]}]
+    revenueData =  FilterData(revenueFilter,fileData)
+    revenueTotal = CalcualteTotalValue(revenueData)
 
-    #print(CalcualteTotalValue(revenueFilteredData))
-    #print(CalcualteTotalValue(expensesFilteredData))
+    #Calculate Expenses
+    expensesFilter = [{"account_category":["expense"]}]
+    expensesData = FilterData(expensesFilter,fileData)
+    expensesTotal = CalcualteTotalValue(expensesData)
+
+    #Calculate Gross Profit Margin
+    gpmAccountFilter = [{"account_type":["sales"]}]
+    gpmValueFilter = [{"value_type":["debit"]}]
+
+    gpmAccountData = FilterData(gpmAccountFilter,fileData)
+    gpmValueData = FilterData(gpmValueFilter,fileData)
+
+    gpmPercent = (CalcualteTotalValue(gpmAccountData) + CalcualteTotalValue(gpmValueData)) / revenueTotal
+    #Calculate Net Profit Margin
+
+    #Calcuate Working Capital Ratio
+    wcrAssetsDebitFilter = [{"account_category":["assets"]},
+    {"value_type":["debit"]},
+    {"account_type":["current","bank","current_accounts_receivable"]}]
+    wcrAssetsCreditFilter = [{"account_category":["assets"]},
+    {"value_type":["credit"]},
+    {"account_type":["current","bank","current_accounts_receivable"]}]
+    wcrLiabilitiesCreditFilter = [{"account_category":["liability"]},
+    {"value_type":["debit"]},
+    {"account_type":["current","current_accounts_payable"]}]
+    wcrLiabilitiesDebitFilter = [{"account_category":["liability"]},
+    {"value_type":["credit"]},
+    {"account_type":["current","current_accounts_payable"]}]
+
+    wcrAssetsDebitData = FilterData(wcrAssetsDebitFilter,fileData)
+    wcrAssetsCreditData = FilterData(wcrAssetsCreditFilter,fileData)
+    wcrLiabilitiesCreditData = FilterData(wcrLiabilitiesCreditFilter,fileData)
+    wcrLiabilitiesDebitData = FilterData(wcrLiabilitiesDebitFilter,fileData)
+
+    wcrAssetsTotal = CalcualteTotalValue(wcrAssetsDebitData) - CalcualteTotalValue(wcrAssetsCreditData)
+    wcrLiabilitiesTotal = CalcualteTotalValue(wcrLiabilitiesCreditData) - CalcualteTotalValue(wcrLiabilitiesDebitData)
 
 def ReadJsonFile(jsonFile):
     with open('data.json') as f:
         return json.load(f)
 
 #Return filtered data relevant to paramaters provided
-def FilterData(key,value,dataList):
+def FilterData(filterOptions,dataList):
     filteredList = list()
     for dataDict in dataList:
-        if dataDict[key] == value:
+        correctlyFiltered = True
+        for filterOption in filterOptions:
+            #Get a list of values from the filter options and check if the data matches if it meet all condtions add it to the filtered data list
+            dictValue = dataDict[list(filterOption.keys())[0]]
+            filterValues = list(filterOption.values())[0]
+            if not (dictValue in filterValues):                
+                correctlyFiltered = False
+                break
+        if correctlyFiltered:
             filteredList += [dataDict]
     return filteredList
 
