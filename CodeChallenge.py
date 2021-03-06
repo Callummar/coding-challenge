987f19c6399b619b50c7d723820c8d92f4937847
Callum Mar <30305310+Callummar@users.noreply.github.com>
2021-03-06 13:57:24 +1300

    Update CodeChallenge.py
    add FilterData function to filter given data on parameters

--- CodeChallenge.py
+++ CodeChallenge.py
@@ -2,10 +2,25 @@ import json
 
 def main():
     jsonFileName = "data.json"
-    fileContents = readJsonFile(jsonFileName)
+    fileContents = ReadJsonFile(jsonFileName)
+    fileData = fileContents["data"]
+    
+    
+    revenueFilteredData =  FilterData("account_category","revenue",fileData)
+    expensesFilteredData = FilterData("account_category","expense",fileData)
 
-def readJsonFile(jsonFile):
+
+def ReadJsonFile(jsonFile):
     with open('data.json') as f:
         return json.load(f)
+
+#Return filtered data relevant to paramaters provided
+def FilterData(key,value,dataList):
+    filteredList = list()
+    for dataDict in dataList:
+        if dataDict[key] == value:
+            filteredList += [dataDict]
+    return filteredList
+
 
 main()
