51b3f7727acd09f615c23c00ba5d6026f948da43
Callum Mar <30305310+Callummar@users.noreply.github.com>
2021-03-06 14:02:30 +1300

    Update CodeChallenge.py
    add CalculateTotalValue function

--- CodeChallenge.py
+++ CodeChallenge.py
@@ -9,6 +9,8 @@ def main():
     revenueFilteredData =  FilterData("account_category","revenue",fileData)
     expensesFilteredData = FilterData("account_category","expense",fileData)
 
+    #print(CalcualteTotalValue(revenueFilteredData))
+    #print(CalcualteTotalValue(expensesFilteredData))
 
 def ReadJsonFile(jsonFile):
     with open('data.json') as f:
@@ -22,5 +24,14 @@ def FilterData(key,value,dataList):
             filteredList += [dataDict]
     return filteredList
 
+#Sum all Total_values based on data provided
+def CalcualteTotalValue(dataList):
+    totalValue = 0
+    for data in dataList:
+        try:
+            totalValue += int(data["total_value"])
+        except:
+            totalValue += 0
+    return totalValue
 
 main()
